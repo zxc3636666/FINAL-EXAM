61f82820bd080eb586477b11cf7774c872afad29
zxc3636666 <32055831+zxc3636666@users.noreply.github.com>
2018-06-10 21:50:05 +0800

    Add files via upload

---
+++ src/Frame.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Frame extends JFrame {
+    private JButton jbt = new JButton("START");
+    private JButton jbt1 = new JButton("EXIT");
+    private JLabel jlb = new JLabel("說明");
+    private JLabel jb1 = new JLabel( );
+    private JTextArea jta = new JTextArea();
+    private JPanel jpn = new JPanel(new GridLayout(2,1,3,3));
+    private JPanel jpn1 = new JPanel(new GridLayout(2,1,3,3));
+    private JPanel jpn2 = new JPanel(new GridLayout(2,1,3,3));
+    private ImageIcon img1 = new ImageIcon("下載.jpg");
+    private Timer t1;
+    private Container cp;
+    private  int count =0;
+
+    public Frame(){
+        init();
+    }
+    private void init(){
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setBounds(0,0,800,600);
+        cp = this.getContentPane();
+        jb1.setBounds(100,200,40,40);
+        jb1.setIcon(img1);
+        jpn.add(jbt);
+        jpn.add(jbt1);
+        jpn1.add(jlb);
+        jpn1.add(jta);
+        jpn2.add(jb1);
+        cp.add(jpn,BorderLayout.WEST);
+        cp.add(jpn1,BorderLayout.EAST);
+        cp.add(jpn2,BorderLayout.CENTER);
+
+        t1 = new Timer(100, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+               }
+           });
+        jbt.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                count = count + 1;
+                Frame.this.setTitle(Integer.toString(count));
+                t1.start();
+            }
+        });
+
+
+        jbt1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Frame frm = new Frame();
+//       frm.setSize(400,500);
+        frm.setVisible(true);
+//       frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
